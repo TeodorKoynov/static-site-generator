8672d891af1e28dc3e36df653cf594973c6e941b
Teodor Koynov <teodor.koynov2005@gmail.com>
2025-12-31 14:39:36 +0200

    add LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,19 @@ class HTMLNode:
     #         return f"<{self.tag}{html_props}{value}>{children}</{self.tag}>"
     #
     #     return create_tag(self.tag, self.props_to_html(), self.children, self.value)
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 from textnode import TextNode, TextType
 
 
 def main():
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     html_node = HTMLNode("p", None, "this is a paragraph", {"custom-tag": "true"})
+    leaf_node = LeafNode("p", "Hello, world!", {"custom-tag": "true"})
 
-    print(html_node)
+    print(leaf_node.to_html())
 
 
 if __name__ == '__main__':
