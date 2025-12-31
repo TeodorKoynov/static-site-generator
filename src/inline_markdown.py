467945d28ecde87338da31bc752c9fda976456bf
Teodor Koynov <teodor.koynov2005@gmail.com>
2025-12-31 15:31:14 +0200

    add split_nodes_delimiter

---
+++ src/inline_markdown.py
@@ -0,0 +1,30 @@
+from textnode import TextNode, TextType
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        if delimiter not in text:
+            new_nodes.append(node)
+            continue
+
+        split_nodes = []
+        sections = text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError(f"Invalid Markdown: unmatched delimiter '{delimiter}' in '{text}'")
+
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
